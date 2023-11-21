d0d1b8273bc8f605bf30efb3f58961a4b5252724
Raymuss423 <cwchastain@gmail.com>
2023-11-21 15:11:03 -0700

    rewrite function to single line

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,9 @@
 const reverseString = function(string) {
- let letters = string.split('');
+ /* let letters = string.split('');
  let reversed = letters.reverse();
- return reversed.join('')
+ return reversed.join('') */
+
+ return string.split('').reverse().join('')
 };
 
 // Do not edit below this line
