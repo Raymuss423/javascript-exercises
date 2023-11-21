7c48f5c5c900160e41e0d06a2d147ae6d3618937
Raymuss423 <cwchastain@gmail.com>
2023-11-21 15:04:52 -0700

    create function to satisfy reverseString test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+ let letters = string.split('');
+ let reversed = letters.reverse();
+ return reversed.join('')
 };
 
 // Do not edit below this line
